cb6f0c1ea7de062a8f00d18bd403d57769b4b389
Tim Moreton <tmoreton89@gmail.com>
2026-04-21 13:10:09 -0400

    Initial Commit

---
+++ yaprflow-claude/yaprflow_claudeApp.swift
@@ -0,0 +1,17 @@
+//
+//  yaprflow_claudeApp.swift
+//  yaprflow-claude
+//
+//  Created by Tim Moreton on 4/21/26.
+//
+
+import SwiftUI
+
+@main
+struct yaprflow_claudeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
